edfc74c743bafa0e56a7cba15e7624945ae529f0
ingeborg <ingeborg.gjerde@gmail.com>
2022-06-23 11:47:54 +0200

    Implemented a custom assembly of one (1) bifurcation

---
+++ src/real_assembly_experiments.py
@@ -0,0 +1,104 @@
+import networkx as nx
+from fenics import *
+from fenics_graph import *
+from utils import *
+from graph_examples import *
+    
+
+
+def hydraulic_network_with_custom_assembly(G, f=Constant(0), p_bc=Constant(0)):
+    '''
+    Solve hydraulic network model 
+        R q + d/ds p = 0
+            d/ds q = f
+    on graph G, with bifurcation condition q_in = q_out
+    and custom assembly of the bifurcatio condition 
+
+    Args:
+        G (fg.FenicsGraph): problem domain
+        f (df.function): source term
+        p_bc (df.function): neumann bc for pressure
+    '''
+    
+    mesh = G.global_mesh
+
+    # Flux spaces on each segment, ordered by the edge list
+    submeshes = list(nx.get_edge_attributes(G, 'submesh').values())
+    P2s = [FunctionSpace(msh, 'CG', 3) for msh in submeshes] 
+    
+    # Pressure space on global mesh
+    P1 = FunctionSpace(mesh, 'CG', 2) # Pressure space (on whole mesh)
+    
+    ### Function spaces
+    spaces = P2s + [P1]
+    W = MixedFunctionSpace(*spaces) 
+
+
+    # Trial and test functions
+    vphi = TestFunctions(W)
+    qp = TrialFunctions(W)
+
+    # split out the components
+    qs = qp[0:G.num_edges]
+    p = qp[-1]
+
+    vs = vphi[0:G.num_edges]
+    phi = vphi[-1]
+
+
+    ## Assemble variational formulation 
+
+    # Initialize blocks in a and L to zero
+    # (so fenics-mixed-dim does not throw an error)
+    dx = Measure('dx', domain=mesh)
+    a = Constant(0)*p*phi*dx
+    L = Constant(0)*phi*dx
+
+    vecs = [G.jump_vector(q, ix, G.bifurcation_ixs[0]) for ix, q in enumerate(qs)]
+
+    # Assemble edge contributions to a and L
+    for i, e in enumerate(G.edges):
+        
+        msh = G.edges[e]['submesh']
+        vf = G.edges[e]['vf']
+        #res = G.edges[e]['res']
+        
+        dx_edge = Measure("dx", domain = msh)
+        ds_edge = Measure('ds', domain=msh, subdomain_data=vf)
+
+        # Add variational terms defined on edge
+        a += qs[i]*vs[i]*dx_edge        
+        a -= p*G.dds(vs[i])*dx_edge
+        a += phi*G.dds(qs[i])*dx_edge
+
+        # Add boundary condition for inflow/outflow boundary node
+        L += p_bc*vs[i]*ds_edge(BOUN_IN)
+        L -= p_bc*vs[i]*ds_edge(BOUN_OUT)
+    
+    # Solve
+    qp0 = mixed_dim_fenics_solve_custom(a, L, W, mesh, vecs)
+    return qp0
+
+
+
+if __name__ == '__main__':
+    
+    G = make_line_graph(3)
+    G.make_mesh(3)
+    mesh = G.global_mesh
+
+    p_bc = Expression('x[0]', degree=1)
+ 
+    qp0 = hydraulic_network_with_custom_assembly(G, p_bc = p_bc)
+
+    vars = qp0.split()
+    p = vars[-1]
+    
+    q = GlobalFlux(G, vars[0:-1])
+    qi = interpolate(q, VectorFunctionSpace(G.global_mesh, 'DG', 2, G.geom_dim))
+    p = vars[-1]
+    
+    qi.rename('q', '0.0')
+    p.rename('p', '0.0')
+    File('plots/p.pvd')<<p
+    File('plots/q.pvd')<<qi
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,53 @@
+from lib2to3.pytree import convert
 from fenics import *
+import scipy.sparse as sp
+from petsc4py import PETSc
+
+
+def mixed_dim_fenics_solve_custom(a, L, W, mesh, real_rows):
+        
+    # Assemble the system
+    qp0 = Function(W)
+    system = assemble_mixed_system(a == L, qp0)
+    
+    A_list = system[0]
+    rhs_blocks = system[1]
+
+    # Convert our real rows to PetSc
+    rows = [convert_vec_to_petscmatrix(row) for row in real_rows]
+    # and get the transpos
+    rows_T = [PETScMatrix(row.mat().transpose(PETSc.Mat())) for row in rows]
+    
+    zero = zero_PETScMat(1,1)
+    zero_row = zero_PETScMat(1,W.sub_space(2).dim())
+    zero_col = zero_PETScMat(W.sub_space(2).dim(),1)
+    
+    # Add lagrange multipliers
+    A_list_n = A_list[0:3] + [rows_T[0]]   # first row
+    A_list_n += A_list[3:6] + [rows_T[1]]  # second row
+    A_list_n += A_list[6:9] + [zero_col]     # third row  
+    A_list_n += rows  + [zero_row, zero]    # fourth row
+    
+    # Solve the system
+    A_ = PETScNestMatrix(A_list_n) # recombine blocks now with Lagrange multipliers
+    b_ = Vector()
+    rhs_blocks_n = rhs_blocks + [zero_PETScVec(1)]
+    A_.init_vectors(b_, rhs_blocks_n)
+    A_.convert_to_aij() # Convert MATNEST to AIJ for LU solver
+
+    sol_ = Vector(mesh.mpi_comm(), sum([P2.dim() for P2 in W.sub_spaces()]) + 1)
+    solver = PETScLUSolver()
+    solver.solve(A_, sol_, b_)
+
+    # Transform sol_ into qp0 and update qp_
+    dim_shift = 0
+    for s in range(W.num_sub_spaces()):
+        qp0.sub(s).vector().set_local(sol_.get_local()[dim_shift:dim_shift + qp0.sub(s).function_space().dim()])
+        dim_shift += qp0.sub(s).function_space().dim()
+        qp0.sub(s).vector().apply("insert")
+    return qp0
+
+
 
 def mixed_dim_fenics_solve(a, L, W, mesh):
         
@@ -27,6 +76,48 @@ def mixed_dim_fenics_solve(a, L, W, mesh):
         qp0.sub(s).vector().apply("insert")
     return qp0
 
+
+def convert_vec_to_petscmatrix(vec):
+    '''
+    Convert a fenics vector from assemble into 
+    dolfin.cpp.la.PETScMatrix
+    '''
+    
+    # Make sparse
+    sparse_vec = sp.coo_matrix(vec.get_local())
+    #row = sp.coo_matrix(vec)
+    
+    # Init PETSC matrix
+    petsc_mat = PETSc.Mat().createAIJ(size=sparse_vec.shape)
+    petsc_mat.setUp()
+    
+    # Input values from sparse_vec
+    for i,j,v in zip(sparse_vec.row, sparse_vec.col, sparse_vec.data): 
+        petsc_mat.setValue(i,j,v)
+    
+    petsc_mat.assemble()
+    
+    return PETScMatrix(petsc_mat)
+
+def zero_PETScVec(n):
+    '''
+    Make PETScVec with n zeros 
+    '''
+    
+    petsc_vec = PETSc.Vec().createSeq(n)
+    return PETScVector(petsc_vec)
+
+
+def zero_PETScMat(n,m):
+    '''
+    Make n,m PETScMatrix with zeros
+    '''
+        
+    petsc_mat = PETSc.Mat().createAIJ((n,m))
+    petsc_mat.setUp()
+    petsc_mat.assemble()
+    
+    return PETScMatrix(petsc_mat)
 
 
 def assemble_global_flux(qs, G):

19c41e8c369eac58efe819b30ee18686909b3fdd
ingeborg <ingeborg.gjerde@gmail.com>
2022-07-29 11:19:54 +0200

    Made function that assigns radius info according to Murray's law

--- src/graph_utils.py
+++ src/graph_utils.py
@@ -1,12 +1,15 @@
 from fenics import *
 import networkx as nx
+import numpy as np
 
 def assign_radius_using_Murrays_law(G, start_node,  start_radius):
     '''
-    Murray's law states that the thickness of branches in transport networks
-    follow the relation
+    Assign radius information using Murray's law, which states that 
+    thickness of branches in transport networks follow the relation
         r_p^3 = r_d1^3 + r_d2^3 + ... + r_dn^3
     where r_p is the radius of the parent and r_di is the radius of daughter vessel i.
+    The relative thickness of the daughter vessels is assigned proportional
+    to the length of the subnetwork sprouting from it.
     
     Args:
         G: graph representing network
@@ -24,6 +27,11 @@ def assign_radius_using_Murrays_law(G, start_node,  start_radius):
     G_ = nx.bfs_tree(G, source=start_node)
     for i in range(0, len(G.nodes)): 
         G_.nodes()[i]['pos'] = G.nodes()[i]['pos']
+        
+    for e in list(G_.edges()):
+        v1, v2 = e
+        length = np.linalg.norm( np.asarray(G_.nodes()[v2]['pos'])-np.asarray(G_.nodes()[v1]['pos']))
+        G_.edges()[e]['length']=length
     
     assert len(list(G.edges(start_node))) is 1, 'start node has to have a single edge sprouting from it'
     
@@ -40,18 +48,36 @@ def assign_radius_using_Murrays_law(G, start_node,  start_radius):
             edge_in = list(G_.in_edges(v1))[0]
             radius_p = G_.edges()[edge_in]['radius']
 
-            # find out how many edges the parent splits into
-            connected_edges = G_.out_edges(v1)
-            num_connected_edges = len(connected_edges) + DOLFIN_EPS 
-            # TODO: why do we sometimes divide by zero here?
+            
+            # Compute the length fraction of the subgraph sprouting from this 
+            # daughters edge
+            
+            # find the subtree starting from v1 and then v2
+            sub_graphs = {}
+            sub_graph_lengths = {}
+            for v in [v2, v1]:
+                sub_graph = G_.subgraph(nx.shortest_path(G_, v))
+                
+                sub_graph_length = 0
+                for d_e in sub_graph.edges():
+                    sub_graph_length += sub_graph.edges()[d_e]["length"]
+                
+                sub_graphs[str(v)] = sub_graph
+                sub_graph_lengths[str(v)] = sub_graph_length
+            
+            sub_graph_lengths[str(v2)] += G_.edges()[e]["length"]
+            
+            fraction = sub_graph_lengths[str(v2)]/sub_graph_lengths[str(v1)]   
+            
+            if sub_graph_lengths[str(v2)] is 0: # terminal edge
+                fraction = 1/len(sub_graphs[str(v1)].edges())
+                
             
             # Now n*radius_dn**3 = radius_p**3 -> radius_dn = (1/n)**(1/3) radius_p
-            radius_d = (1/num_connected_edges)**(1/3)*radius_p
-            print(e, connected_edges, num_connected_edges, radius_p, radius_d)
+            radius_d = (fraction)**(1/3)*radius_p
             G_.edges()[e]['radius']=radius_d
             
     return G_
-
 
 
 def test_Murrays_law_on_double_bifurcation():
@@ -62,17 +88,19 @@ def test_Murrays_law_on_double_bifurcation():
     G = assign_radius_using_Murrays_law(G, start_node=0, start_radius=1)
         
     for v in G.nodes():
-    es_in = list(G.in_edges(v))
-    es_out = list(G.out_edges(v))
-    
-    # no need ot check terminal edges
-    if len(es_out) is 0 or len(es_in) is 0: continue 
-    
-    r_p_cubed, r_d_cubed = 0, 0
-    for e_in in es_in:
-        r_p_cubed += G.edges()[e_in]['radius']**3
-    for e_out in es_out:
-        r_d_cubed += G.edges()[e_out]['radius']**3
-    
-    assert near(r_p_cubed, r_d_cubed, 1e-6), 'Murrays law not satisfied'
-    
+        es_in = list(G.in_edges(v))
+        es_out = list(G.out_edges(v))
+        
+        # no need to check terminal edges
+        if len(es_out) is 0 or len(es_in) is 0: continue 
+        
+        r_p_cubed, r_d_cubed = 0, 0
+        for e_in in es_in:
+            r_p_cubed += G.edges()[e_in]['radius']**3
+        for e_out in es_out:
+            r_d_cubed += G.edges()[e_out]['radius']**3
+        
+        print(v, r_p_cubed, r_d_cubed)
+        
+        assert near(r_p_cubed, r_d_cubed, 1e-6), 'Murrays law not satisfied'
+    return G

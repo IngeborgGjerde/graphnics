5aec283fc5596a5b20d87d65da0cc7fe41b56315
ingeborg <ingeborg.gjerde@gmail.com>
2022-11-04 23:20:05 +0100

    Add parameters file

---
+++ graphnics/flowmodels/parameters.py
@@ -0,0 +1,7 @@
+from fenics import *
+from networkx import radius
+import numpy as np
+
+
+# viscosity and density of water, using SI units with mm!!
+water_properties = {'nu': 0.697, 'rho': 1e-3, 'mu':0.697*1e-3}

a07b5b00266caf34ee874718bdacb362d81097be
ingeborg <ingeborg.gjerde@gmail.com>
2023-03-22 12:01:33 +0100

    Add NetworkStokes back

--- graphnics/flowmodels/flow_models.py
+++ graphnics/flowmodels/flow_models.py
@@ -205,7 +205,7 @@ class MixedHydraulicNetwork:
 
         # split out the components
         n_edges = G.num_edges
-
+        
         qs, vs = qp[:n_edges], vphi[:n_edges]
         p, phi = qp[n_edges], vphi[n_edges]
         lams, xis = qp[n_edges+1:], vphi[n_edges+1:]
@@ -343,3 +343,82 @@ class MixedHydraulicNetwork:
         
         return qp
 
+
+class NetworkStokes(MixedHydraulicNetwork):
+    
+    def __init__(self, G, f=Constant(0), g = Constant(0), p_bc=Constant(0), nu=Constant(1), space=RT):
+        self.nu = nu
+        
+        super().__init__(G=G, f=f, g=g, p_bc=p_bc, space=space)
+        
+    def a_form(self, a=None):
+        """
+        Add edge contributions to the bilinear form
+        """
+
+        if not a:
+            a = self.init_a_form()
+
+        qp, vphi = self.qp, self.vphi
+        G = self.G
+        
+
+        # split out the components
+        n_edges = G.num_edges
+        
+        qs, vs = qp[:n_edges], vphi[:n_edges]
+        p, phi = qp[n_edges], vphi[n_edges]
+        lams, xis = qp[n_edges+1:], vphi[n_edges+1:]
+
+        # get submeshes and restriction of p to edge
+        submeshes = list(nx.get_edge_attributes(G, "submesh").values())
+        ps = [Restriction(p, msh) for msh in submeshes]
+        phis = [Restriction(phi, msh) for msh in submeshes]
+
+        # add edge terms
+        for i, e in enumerate(G.edges):
+            dx_edge = Measure("dx", domain=G.edges[e]["submesh"])
+            
+            try: 
+                res = self.G.edges()[e]["Res"]
+            except KeyError:
+                res = Constant(1)
+                
+            try: 
+                Ainv = self.G.edges()[e]["Ainv"]
+            except KeyError:
+                Ainv = Constant(1)
+
+
+            a[i][i] += res * qs[i] * vs[i] * dx_edge + (self.mu/Ainv)*G.dds_i(qs[i], i) * G.dds_i(vs[i], i) * dx_edge
+            
+            a[n_edges][i] += +G.dds_i(qs[i], i) * phis[i] * dx_edge
+            a[i][n_edges] += -ps[i] * G.dds_i(vs[i], i) * dx_edge
+            
+            
+        # add vertex terms
+        edge_list = list(G.edges.keys())
+        
+        for b_ix, b in enumerate(G.bifurcation_ixs):
+
+            # make info dict of edges connected to this bifurcation
+            conn_edges = {
+                **{e: (1, BIF_IN, edge_list.index(e)) for e in G.in_edges(b)},
+                **{e: (-1, BIF_OUT, edge_list.index(e)) for e in G.out_edges(b)},
+            }
+
+            for e in conn_edges:
+                ds_edge = Measure(
+                    "ds", domain=G.edges[e]["submesh"], subdomain_data=G.edges[e]["vf"]
+                )
+
+                sign, tag, e_ix = conn_edges[e]
+
+                a[e_ix][n_edges + 1 + b_ix] += (
+                    sign * vs[e_ix] * lams[b_ix] * ds_edge(tag)
+                )
+                a[n_edges + 1 + b_ix][e_ix] += (
+                    sign * qs[e_ix] * xis[b_ix] * ds_edge(tag)
+                )
+
+        return a
